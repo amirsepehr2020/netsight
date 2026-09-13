ef0bd2e525f0c6a8256c4208487c46cc216190e5
SEPEHR <sepehr2sodoury@gmail.com>
2026-09-13 15:13:23 +0330

    test: add core diagnostic smoke tests

---
+++ core/src/lib.rs
@@ -0,0 +1,18 @@
+pub mod model;
+
+#[cfg(test)]
+mod tests {
+    use super::model::{CaptureState, DiagnosticEvent, DiagnosticLevel};
+
+    #[test]
+    fn diagnostic_event_is_constructible() {
+        let event = DiagnosticEvent::new("CAPTURE-0001", DiagnosticLevel::Info, "capture initialized");
+        assert_eq!(event.code, "CAPTURE-0001");
+        assert_eq!(event.level, DiagnosticLevel::Info);
+    }
+
+    #[test]
+    fn capture_state_starts_idle() {
+        assert_eq!(CaptureState::Idle, CaptureState::default());
+    }
+}
